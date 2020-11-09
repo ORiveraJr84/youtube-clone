da042c16a7992989b69469f92646f7fcb74cfc03
ORiveraJr84 <oriverajr84@gmail.com>
2020-11-09 04:56:47 -0500

    removed unneccesary files and updated gitignore

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJbUfcqHx_RKr_QCM2b491lYjYVUlvQPE",
+  authDomain: "clone-a1516.firebaseapp.com",
+  databaseURL: "https://clone-a1516.firebaseio.com",
+  projectId: "clone-a1516",
+  storageBucket: "clone-a1516.appspot.com",
+  messagingSenderId: "474881404991",
+  appId: "1:474881404991:web:9f9f2fe09006cec276888a",
+  measurementId: "G-VQ1VHQ0K49",
+};
